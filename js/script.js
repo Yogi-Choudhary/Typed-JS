282bcb277a825f1a97aa4efdb62547849f877f54
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2018-12-26 09:22:07 +0530

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".element", {
+    strings: [
+        "Designer", 
+        "Developer",
+        "Freelancer"
+    ],
+    typespeed: 60,
+    backspeed: 60,
+    loop: true
+  });
